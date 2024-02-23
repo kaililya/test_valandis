db1b542f505dd1affaa6150b1f0574d970822b29
kail <ilyaman.zona2033@mail.ru>
2024-02-23 22:20:58 +0300

    fix https v2

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,6 +2,6 @@ export const mainPATH = '/'
 export const modalPATH = '/modal'
 
 export const password = 'Valantis'
-export const mainURL = 'https://api.valantis.store:40000/'
+export const mainURL = 'http://api.valantis.store:40000/'
 export const pageLimit = 50;
 
