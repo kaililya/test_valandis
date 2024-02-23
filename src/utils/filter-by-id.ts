8cab385e4af6ad20da9e74acc3ce1122c2edcfba
kail <ilyaman.zona2033@mail.ru>
2024-02-23 22:11:54 +0300

    fix not used imports v3

--- src/utils/filter-by-id.ts
+++ src/utils/filter-by-id.ts
@@ -14,13 +14,4 @@ export function removeDuplicateById(products:TGood[]) {
   return result;
 }
 
-// Пример использования:
-const productList = [
-  { brand: 'Brand A', id: '001', price: 50, product: 'Product A' },
-  { brand: 'Brand B', id: '002', price: 60, product: 'Product B' },
-  { brand: 'Brand A', id: '001', price: 55, product: 'Product A' },
-  { brand: 'Brand C', id: '003', price: 70, product: 'Product C' },
-  { brand: 'Brand D', id: '004', price: 80, product: 'Product D' },
-  { brand: 'Brand E', id: '002', price: 65, product: 'Product B' },
-];
 
